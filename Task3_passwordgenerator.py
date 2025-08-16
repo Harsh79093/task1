ee5a49e92aea6736b9c163d272264e8d64a540d1
Harsh79093 <harshchauhan72151@gmail.com>
2025-08-16 15:19:04 +0530

    Update and rename passwordgenerator.py to Task3_passwordgenerator.py

--- Task3_passwordgenerator.py
+++ Task3_passwordgenerator.py
@@ -2,14 +2,11 @@ import random
 import string
 
 def generate_password(length):
-    # Define the character set
     characters = string.ascii_letters + string.digits + string.punctuation
-    # Generate a random password
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
 def main():
-    # User input for password length
     while True:
         try:
             length = int(input("Specify the desired length of the password: "))
@@ -19,10 +16,9 @@ def main():
             break
         except ValueError:
             print("Invalid input. Please enter a number.")
-            
-    # Generate and display the password
     password = generate_password(length)
     print("Generated Password:", password)
 
 if __name__ == "__main__":
     main()
+
